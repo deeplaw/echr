4b116afa39d33709caea34cc2f6fa2dd40427ae9
Kasper Marstal <kaspermarstal@gmail.com>
2016-11-03 19:49:00 +0100

    ENH: Add neural network architecture optimization

--- dnn_facts.py
+++ dnn_facts.py
@@ -2,64 +2,91 @@ import pandas
 import numpy
 
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
+from keras.layers import Activation, Dense, Dropout
 from keras.wrappers.scikit_learn import KerasClassifier
+from keras.regularizers import l2
 
-from sklearn.model_selection import cross_val_score, StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, GridSearchCV
 from sklearn.preprocessing import LabelEncoder
-from sklearn.pipeline import Pipeline
 
-# fix random seed for reproducibility
-seed = 7
-numpy.random.seed(seed)
 
 # load dataset
 cases3 = pandas.read_csv('echr_dataset/Article3/cases_a3.csv', header=None)
 circumstances3 = pandas.read_csv('echr_dataset/Article3/ngrams_a3_circumstances.csv', header=None)
 relevantLaw3 = pandas.read_csv('echr_dataset/Article3/ngrams_a3_relevantLaw.csv', header=None)
+full3 = pandas.read_csv('echr_dataset/Article3/ngrams_a3_full.csv', header=None)
+law3 = pandas.read_csv('echr_dataset/Article3/ngrams_a3_law.csv', header=None)
+procedure3 = pandas.read_csv('echr_dataset/Article3/ngrams_a3_procedure.csv', header=None)
+relevantLaw3 = pandas.read_csv('echr_dataset/Article3/ngrams_a3_relevantLaw.csv', header=None)
 
-cases6 = pandas.read_csv('echr_dataset/Article3/cases_a3.csv', header=None)
-circumstances6 = pandas.read_csv('echr_dataset/Article3/ngrams_a3_circumstances.csv', header=None)
-relevantLaw6 = pandas.read_csv('echr_dataset/Article3/ngrams_a3_relevantLaw.csv', header=None)
+cases6 = pandas.read_csv('echr_dataset/Article6/cases_a6.csv', header=None)
+circumstances6 = pandas.read_csv('echr_dataset/Article6/ngrams_a6_circumstances.csv', header=None)
+relevantLaw6 = pandas.read_csv('echr_dataset/Article6/ngrams_a6_relevantLaw.csv', header=None)
 
-cases8 = pandas.read_csv('echr_dataset/Article3/cases_a3.csv', header=None)
-circumstances8 = pandas.read_csv('echr_dataset/Article3/ngrams_a3_circumstances.csv', header=None)
-relevantLaw8 = pandas.read_csv('echr_dataset/Article3/ngrams_a3_relevantLaw.csv', header=None)
+cases8 = pandas.read_csv('echr_dataset/Article8/cases_a8.csv', header=None)
+circumstances8 = pandas.read_csv('echr_dataset/Article8/ngrams_a8_circumstances.csv', header=None)
+relevantLaw8 = pandas.read_csv('echr_dataset/Article8/ngrams_a8_relevantLaw.csv', header=None)
 
-cases = pandas.concat([cases3, cases6, cases8])
-circumstances = pandas.concat([circumstances3, circumstances6, circumstances8])
-relevantLaw = pandas.concat([relevantLaw3, relevantLaw6, relevantLaw8])
+x = pandas.concat((circumstances3, relevantLaw3), axis=1)
+y = cases3[1]
 
-x = circumstances+relevantLaw
-y = cases[1]
-
-# shuffle and convert to numpy arrays
-perm = numpy.random.permutation(numpy.arange(len(x)))
-x_shuffled = x.iloc[perm].as_matrix()
-y_shuffled = y.iloc[perm].values
+x = x.as_matrix()
+y = y.values
 
 # encode verdicts as integers
 encoder = LabelEncoder()
-encoder.fit(y_shuffled)
-encoded_shuffled_y = encoder.transform(y_shuffled)
+encoder.fit(y)
+encoded_y = encoder.transform(y)
 
-# model architecture
-def DNN():
+
+# neural network architecture
+def build_fn(n_hidden_units, init, W_regularization_weight, b_regularization_weight, activation, dropout, optimizer):
     model = Sequential()
-    model.add(Dense(256, input_dim=x.shape[1], init='normal', activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(64, init='normal', activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(8, init='normal', activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1, init='normal', activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    # input layer
+    model.add(Dense(n_hidden_units[0], init=init, input_dim=x.shape[1], W_regularizer=l2(W_regularization_weight), b_regularizer=l2(b_regularization_weight)))
+    model.add(Activation(activation))
+    model.add(Dropout(dropout))
+
+    # hidden layers
+    for n_hidden_unit in n_hidden_units[1:]:
+        model.add(Dense(n_hidden_unit, init=init, W_regularizer=l2(W_regularization_weight), b_regularizer=l2(b_regularization_weight)))
+        model.add(Activation(activation))
+        model.add(Dropout(dropout))
+
+    # output layer
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+
+    # build network
+    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
     return model
 
-estimators = []
-estimators.append(('mlp', KerasClassifier(build_fn=DNN, nb_epoch=64, batch_size=32, verbose=1)))
-pipeline = Pipeline(estimators)
-kfold = StratifiedKFold(n_splits=2, shuffle=True, random_state=seed)
-results = cross_val_score(pipeline, x_shuffled, encoded_shuffled_y, cv=kfold, verbose=1)
-print("Result: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
 
+# architecture parameters
+param_grid = dict(
+    nb_epoch=numpy.array([32]),
+    batch_size=numpy.array([8]),
+    n_hidden_units=[[64, 64, 64], ],  # [64, 64, 64]
+    init=['uniform'],
+    W_regularization_weight=[0.000002, ],
+    b_regularization_weight=[0.00002, ],
+    activation=['relu'],
+    dropout=[0.5],
+    optimizer=['adam'])
+
+
+# fix random seed for reproducibility
+seed = 8
+
+
+# search parameter space for best architecture
+classifier = KerasClassifier(build_fn=build_fn, verbose=1)
+skfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+grid_search = GridSearchCV(classifier, param_grid, cv=skfold, verbose=1)
+grid_result = grid_search.fit(x, encoded_y)
+
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+for params, mean_score, scores in grid_result.grid_scores_:
+    print("%f (%f) with: %r" % (scores.mean(), scores.std(), params))
