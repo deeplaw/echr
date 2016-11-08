2653f28a9b1dbf995999cc751bfc1e3a1563d92c
Kasper Marstal <kaspermarstal@gmail.com>
2016-11-08 11:24:54 +0100

    ENH: Also tune regularization parameters

--- dnn_facts.py
+++ dnn_facts.py
@@ -5,6 +5,7 @@ from keras.models import Sequential
 from keras.layers import Activation, Dense, Dropout
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.regularizers import l2
+from keras.callbacks import EarlyStopping, TensorBoard
 
 from sklearn.model_selection import StratifiedKFold, GridSearchCV
 from sklearn.preprocessing import LabelEncoder
@@ -27,10 +28,10 @@ cases8 = pandas.read_csv('echr_dataset/Article8/cases_a8.csv', header=None)
 circumstances8 = pandas.read_csv('echr_dataset/Article8/ngrams_a8_circumstances.csv', header=None)
 relevantLaw8 = pandas.read_csv('echr_dataset/Article8/ngrams_a8_relevantLaw.csv', header=None)
 
-x = pandas.concat((circumstances3, relevantLaw3), axis=1)
+X = pandas.concat((circumstances3, relevantLaw3), axis=1)
 y = cases3[1]
 
-x = x.as_matrix()
+X = X.as_matrix()
 y = y.values
 
 # encode verdicts as integers
@@ -44,7 +45,7 @@ def build_fn(n_hidden_units, init, W_regularization_weight, b_regularization_wei
     model = Sequential()
 
     # input layer
-    model.add(Dense(n_hidden_units[0], init=init, input_dim=x.shape[1], W_regularizer=l2(W_regularization_weight), b_regularizer=l2(b_regularization_weight)))
+    model.add(Dense(n_hidden_units[0], init=init, input_dim=X.shape[1], W_regularizer=l2(W_regularization_weight), b_regularizer=l2(b_regularization_weight)))
     model.add(Activation(activation))
     model.add(Dropout(dropout))
 
@@ -66,22 +67,27 @@ def build_fn(n_hidden_units, init, W_regularization_weight, b_regularization_wei
 
 # neural network parameters
 param_grid = dict(
-    nb_epoch=numpy.array([32]),
+    nb_epoch=numpy.array([64]),
     batch_size=numpy.array([8]),
     n_hidden_units=[[64, 64, 64], ],  # [64, 64, 64]
     init=['uniform'],
-    W_regularization_weight=[0.000002, ],
-    b_regularization_weight=[0.00002, ],
+    W_regularization_weight=[0.000003, 0.0002],
+    b_regularization_weight=[0.00003, ],
     activation=['relu'],
     dropout=[0.5],
     optimizer=['adam'])
 
+# Tensor Board
+tensor_board = TensorBoard(log_dir='./TensorBoard')
 
-# search parameter space for best neural network architecture, fix random state for reproducibility
+# Early stopping
+early_stopping = EarlyStopping(monitor='acc', patience=8, verbose=1)
+
+# search parameter space for best neural network architecture, fix internal random state for reproducibility
 classifier = KerasClassifier(build_fn=build_fn, verbose=1)
-skfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=8)
-grid_search = GridSearchCV(classifier, param_grid, cv=skfold, verbose=1)
-grid_result = grid_search.fit(x, encoded_y)
+skfold = StratifiedKFold(n_splits=2, shuffle=True, random_state=8)
+grid_search = GridSearchCV(classifier, param_grid, cv=skfold, verbose=1, fit_params={'callbacks': [tensor_board, early_stopping]})
+grid_result = grid_search.fit(X, encoded_y)
 
 print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
 for params, mean_score, scores in grid_result.grid_scores_:
